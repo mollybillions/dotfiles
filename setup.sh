57ae0d00bc25c007b09c800e774f68f5adb5724a
Molly Brasington <68570851+mollybillions@users.noreply.github.com>
2021-06-23 11:00:02 -0400

    Setup reportify for spin
    Try autoloading a cartridge for reportify support.

--- setup.sh
+++ setup.sh
@@ -70,5 +70,16 @@ cp ~/dotfiles/.zshrc ~/.zshrc
 cp ~/dotfiles/.gitconfig ~/.gitconfig
 cp ~/dotfiles/.tmux.conf ~/.tmux.conf
 
+echo "Reportify setup..."
+# Only run nested steps in Spin + shopify/shopify workspaces.
+if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
+then
+  cd "$SPIN_REPO_SOURCE_PATH"
+  # This will always be the author of the cartridge, so for the shopify-reportify-config it will be shameelabd (do not modify this)
+  # Do **NOT** replace this username with your username.
+  cartridge insert shameelabd/shopify-reportify-config
+  . /cartridges/shopify-reportify-config/setup.sh
+  restart
+  fi
 
 echo "DONE!"
