fe33ac018c96d9555ab3745cf363e931ecd15228
Molly Brasington <68570851+mollybillions@users.noreply.github.com>
2021-06-26 21:20:24 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -72,7 +72,7 @@ fi
 #  sudo apt-get install -y fd
 #fi
 
-if ! command_exists delta: then
+if ! command_exists delta; then
   local tmp_deb="$(mktemp)"
   local src_url="https://github.com/barnumbirr/delta-debian/releases/download/0.6.0-1/delta-diff_0.6.0-1_amd64_debian_testing.deb"
 
